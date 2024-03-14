3b7f264a4422b5475c2a1ab54bf59226b1e6620f
choshibi <115560399+aroarowww@users.noreply.github.com>
2024-03-14 12:09:43 +0900

    課題

--- src/lesson2/FizzBuzz.java
+++ src/lesson2/FizzBuzz.java
@@ -3,8 +3,22 @@ package lesson2;
 public class FizzBuzz {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+//		1から100までの数値をループで処理する
+//		3で割り切れる数値の場合、「Fizz」と表示する
+//		5で割り切れる数値の場合、「Buzz」と表示する
+//		3と5の両方で割り切れる数値の場合、「FizzBuzz」と表示する
+//		それ以外の数値は、その数値をそのまま表示する
+		for (int i=1; i<=100; i++) {
+			if (i%3 == 0) {
+				System.out.println("Fizz");
+			}else if(i%5==0){
+				System.out.println("Buzz");
+			}else if((i%3 == 0)&&(i%5 == 0)){
+				System.out.println("FizzBuzz");
+			}else {
+				System.out.println(i);
+			}
+		}
 	}
 
 }
